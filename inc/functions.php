74fe8e818f615be615a152c630c1ce8503347ef5
Pedro HGB Queiroz <barrosoph@gmail.com>
2017-03-21 16:22:36 +0100

    - Added functions (single movie and number of pages) - Added catalog view and public functions - Fixed forms - Created editmovie pages

--- inc/functions.php
+++ inc/functions.php
@@ -1,19 +1,17 @@
 <?php
 
 // this function will allow us to select all the available movie information in the database
-function getInfo($search='') {
+function infoAllMovies($search='', $nbPerPage=0, $offset=0) {
     global $pdo;
 
     $movieInfoSelect = '
-        SELECT mov_title, mov_info, mov_path, cat_title, mov_poster
+        SELECT *
         FROM movie
         INNER JOIN categories ON categories.cat_id = movie.categories_cat_id
-        /*INNER JOIN support ON support.sup_id = movie.support_cat_id*/
     ';
 
     //displaying the search
     if (!empty($search)) {
-        //$displayPagination = false;
         $movieInfoSelect .= '
             WHERE mov_title LIKE :search
             OR cat_title LIKE :search
@@ -22,6 +20,12 @@ function getInfo($search='') {
         ';
     }
 
+    if ($nbPerPage > 0) {
+        $movieInfoSelect .= '
+            LIMIT '.$nbPerPage.' OFFSET '.$offset.'
+        ';
+    }
+ 
     $request = $pdo->prepare($movieInfoSelect);
 
     if (!empty($search)) {
@@ -37,4 +41,52 @@ function getInfo($search='') {
 
     return false;
    
-} // function selectMovieInfo end
+}
+
+function infoOneMovie ($movieId){
+    global $pdo;
+
+    $oneMovieInfoSelect = '
+        SELECT *
+        FROM movie
+        INNER JOIN categories ON categories.cat_id = movie.categories_cat_id
+        INNER JOIN support ON support.sup_id = movie.support_cat_id
+    ';
+
+
+    if (!empty($movieId)) {
+        $oneMovieInfoSelect .= ' WHERE mov_id = :movieId';
+    }
+
+    $request = $pdo->prepare($oneMovieInfoSelect);
+
+    if (!empty($movieId)) {
+        $request->bindValue(':movieId', $movieId, PDO::PARAM_INT);
+    }
+
+    if ($request->execute() === false ) {
+        print_r( $request->errorInfo() );
+    } else {
+        $oneMovieInfo = $request->fetch(PDO::FETCH_ASSOC);
+        return $oneMovieInfo;
+    }
+
+    return false;
+}
+
+function totalMovies(){
+    global $pdo;
+    $oneMovieInfoSelect = '
+        SELECT *
+        FROM movie
+    ';
+
+    $request = $pdo->prepare($oneMovieInfoSelect);
+
+    if ($request->execute() === false ) {
+        print_r( $request->errorInfo() );
+    }else {
+        return $request->rowCount();
+    }
+    return false;
+}
--- view/catalog.php
+++ view/catalog.php
@@ -1,17 +1,49 @@
 <main>
+    <!-- Displays a movie for each matching result -->
+    <?php foreach($movieInfo as $movie) : ?>
     <div id="flexboxCat">
+
         <div id="posterCat">
-            <img src="<?= $movieInfo['mov_poster'] ?>" alt="<?= $movieInfo['mov_title']?> Poster">
+            <!-- Links poster to detail page -->
+            <a href="moviedetail.php?movieId=<?=$movie['mov_id'] ?>">
+                <!-- Grabs poster -->
+                <img src="<?= $movie['mov_poster'] ?>" alt="<?= $movie['mov_title']?> Poster">
+            </a>
         </div>
 
         <div id="movieInfoCat">
-            <h3><?= $movieInfo['mov_title'] ?></h3>
-            <p><?= $movieInfo['mov_info'] ?></p>
+            <!-- Links title to detail page -->
+            <a href="moviedetail.php?movieId=<?=$movie['mov_id'] ?>">
+                <h3><?= $movie['mov_title'] ?></h3>
+            </a>
+
+            <!-- Grabs movie info -->
+            <p><?= $movie['mov_info'] ?></p>
         </div>
 
         <div id="buttonsCat">
-            <button type="button" name="details" id="details"><a href="#">Details</a></button>
-            <button type="button" name="edit" id="edit"><a href="#">Edit</a></button>
+            <!-- Links button to detail page -->
+            <button type="button" name="details" id="details">
+                <a href="moviedetail.php?movieId=<?=$movie['mov_id'] ?>">Details</a>
+            </button>
+            <!-- Links button to edit page -->
+            <button type="button" name="edit" id="edit">
+                <!-- Sends movie id to edit page -->
+                <a href="editmovie.php?movieId=<?=$movie['mov_id'] ?>">Edit</a>
+            </button>
         </div>
     </div>
+    <?php endforeach; ?>
+
+    <?php if($page>1): ?>
+        <button>
+            <a href="?page=<?= $page-1?>">Previous</a>
+        </button>
+    <?php endif; ?>
+
+    <?php if ($page*$nbPerPage<totalMovies()): ?>
+        <button>
+            <a href="?page=<?= $page+1?>">Next</a>
+        </button>
+    <?php endif; ?>
 </main>
